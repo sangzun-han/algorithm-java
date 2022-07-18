ca5c5a9f564ed01402a10d5886fc1f0bc3603c83
sangzun <hsjun1996@naver.com>
2022-07-18 22:46:21 +0900

    boj10809 2743

---
+++ src/alg/Boj2743.java
@@ -0,0 +1,11 @@
+package alg;
+
+import java.util.Scanner;
+
+public class Boj2743 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.nextLine();
+		System.out.println(word.length());
+	}
+}
