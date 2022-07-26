c165c3ced7b9db3f95f6ef87db830d9d353ba047
sangzun <hsjun1996@naver.com>
2022-07-26 22:25:34 +0900

    boj 1316 1934 2609 2941

---
+++ src/alg/Boj1316.java
@@ -0,0 +1,25 @@
+package alg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj1316 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int ans = n;
+		for(int i=0; i<n; i++) {
+			String word = br.readLine();
+			for(int j=1; j<word.length(); j++) {
+				System.out.println(word.indexOf(word.charAt(j-1)));
+				System.out.println(word.indexOf(word.charAt(j)));
+				if(word.indexOf(word.charAt(j-1)) > word.indexOf(word.charAt(j))) {
+					ans--;
+					break;
+				}
+			}
+		}
+		System.out.println(ans);
+	}
+}
