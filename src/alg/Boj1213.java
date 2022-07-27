2f628f7016875a18adfc01874d12b8490d3be6d6
sangzun <hsjun1996@naver.com>
2022-07-27 22:17:15 +0900

    boj 1213 1543

---
+++ src/alg/Boj1213.java
@@ -0,0 +1,44 @@
+package alg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj1213 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String word = br.readLine();
+		int[] alpha = new int[26];
+		String ans = "";
+		int oddCount = 0;
+		for(int i=0; i<word.length(); i++) {
+			alpha[word.charAt(i)-'A'] += 1;
+		}
+		
+		for(int i=0; i<alpha.length; i++) {
+			if(alpha[i]%2!=0) oddCount++;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		if(oddCount > 1) System.out.println("I'm Sorry Hansoo");
+		else {
+			for(int i=0; i<alpha.length; i++) {
+				for(int j=0; j<alpha[i]/2; j++) {
+					sb.append((char)(i+65));
+				}
+			}
+			ans += sb.toString();
+			String end = sb.reverse().toString();
+			
+			sb = new StringBuilder();
+			for(int i=0; i<alpha.length; i++) {
+				if(alpha[i]%2==1) {
+					sb.append((char)(i+65));
+				}
+			}
+			ans += sb.toString()+end;
+		}
+		System.out.println(ans);
+	}
+}
