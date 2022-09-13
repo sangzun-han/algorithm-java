f4733cf5db6b35e884cd2da93dfbbf651a07a13d
sangzun <hsjun1996@naver.com>
2022-09-13 23:51:40 +0900

    BOJ : 10844

---
+++ src/Main_BJ_10844_쉬운계단수.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_BJ_10844_쉬운계단수 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		
+	}
+}
